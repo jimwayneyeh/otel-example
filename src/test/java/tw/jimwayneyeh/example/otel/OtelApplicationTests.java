0e3c4861dd9c944b08e24ccb8aeed0982b3dd8f1
Wayne Yeh <jimwayne@gmail.com>
2023-01-27 14:03:55 +0800

    chore: initial w/ Spring Initializr

---
+++ src/test/java/tw/jimwayneyeh/example/otel/OtelApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.jimwayneyeh.example.otel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
